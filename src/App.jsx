24364af3381e9a415adf0f53a1cb3cf3d1b5aa23
Mehdishrft <ssheraf2@uwo.ca>
2023-12-16 04:47:47 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import './App.css';
+import { Outlet} from 'react-router-dom';
+import { AuthContext } from './contexts/AuthProvider';
+import Navbar from './pages/shared/Navbar';
+import FooterMain from './pages/shared/FooterMain';
+import { ShoppingCartProvider } from './contexts/ShoppingCartContext';
+import { SearchProvider } from './contexts/SearchContext';
+
+
+
+
+function App() {
+  const { user } = useContext(AuthContext);
+ 
+
+  return (
+    
+    <ShoppingCartProvider>
+      <SearchProvider>
+      <>
+        <Navbar />
+        <div className='min-h-screen'>
+          <Outlet />
+        </div>
+        <FooterMain />
+      </>
+      </SearchProvider>
+      </ShoppingCartProvider>
+  );
+}
+
+export default App;
---
+++ src/Dashboard/EditBooks.jsx
@@ -0,0 +1,246 @@
+import React, { useState } from 'react'
+import { Button, Checkbox, Label, Select, TextInput, Textarea } from 'flowbite-react';
+import { useLoaderData, useParams } from 'react-router-dom';
+
+const EditBooks = () => {
+  const { id } = useParams();
+  const { bookTitle, authorName, imageURL, category, bookDescription, bookPDFURL, price } = useLoaderData();
+  // console.log(bookTitle)
+
+  const bookCategories = [
+    "Fiction",
+    "Non-fiction",
+    "Mystery",
+    "Programming",
+    "Science fiction",
+    "Fantasy",
+    "Horror",
+    "Biography",
+    "Autobiography",
+    "History",
+    "Self-help",
+    "Business",
+    "Memoir",
+    "Poetry",
+    "Children's books",
+    "Travel",
+    "Religion and spirituality",
+    "Science",
+    "Art and design",
+  ];
+
+  const [selectedBookCategory, setSelectedBookCategory] = useState(
+    bookCategories[0]
+  );
+
+  const handleChangeSelectedValue = (event) => {
+    console.log(event.target.value);
+    setSelectedBookCategory(event.target.value);
+  };
+
+
+  const  handleUpdate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    const bookTitle = form.bookTitle.value;
+    const authorName = form.authorName.value;
+    const imageURL = form.imageURL.value;
+    const category = form.categoryName.value;
+    const bookDescription = form.bookDescription.value;
+    const bookPDFURL = form.bookPDFURL.value;
+    const price =form.price.value;
+    window.location.href = "/admin/dashboard/manage";
+
+    const bookObj = {
+      bookTitle,
+      authorName,
+      imageURL,
+      category,
+      bookDescription,
+      bookPDFURL,
+      price,
+    };
+    // console.log(bookObj)
+
+    // update the book object
+    fetch(`https://heroku-create-ff6f90ecc82f.herokuapp.com/book/${id}`, {
+      method: "PATCH",
+
+      headers: {
+        "Content-type": "application/json",
+      },
+
+      body: JSON.stringify(bookObj),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  
+    return (
+      <div className='px-4 my-12'>
+        <h2 className='mb-8 text-3xl font-bold'>Upload A Book!</h2>
+        <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4" onSubmit={handleUpdate}>
+
+          {/* first row */}
+          <div className='flex gap-8'>
+
+            {/* book name */}
+            <div className='lg:w-1/2'>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="bookTitle"
+                  value="Book Title"
+                />
+              </div>
+              <TextInput
+                id="bookTitle"
+                placeholder="Book Name"
+                required
+                type="text"
+                name='bookTitle'
+                className='w-full'
+                defaultValue={bookTitle}
+              />
+            </div>
+
+            {/* author name */}
+            <div className='lg:w-1/2'>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="authorName"
+                  value="Author Name"
+                />
+              </div>
+              <TextInput
+                id="authorName"
+                placeholder="Author Name"
+                required
+                type="text"
+                name='authorName'
+                className='w-full'
+                defaultValue={authorName}
+              />
+            </div>
+
+          </div>
+
+          {/* 2nd Row */}
+          <div className='flex gap-8'>
+            {/* book url */}
+            <div className='lg:w-1/2'>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="imageURL"
+                  value="Book Image URL"
+                />
+              </div>
+              <TextInput
+                id="imageURL"
+                placeholder="Image URL"
+                required
+                type="text"
+                name='imageURL'
+                className='w-full'
+                defaultValue={imageURL}
+              />
+            </div>
+
+            {/* book category */}
+            <div className='lg:w-1/2'>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="inputState"
+                  value="Book Category"
+                />
+              </div>
+              <Select
+                id="inputState"
+                name="categoryName"
+                className="w-full rounded"
+                value={selectedBookCategory}
+                onChange={handleChangeSelectedValue}
+              >
+                {bookCategories.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </Select>
+            </div>
+
+          </div>
+
+          {/* full width div for book description */}
+          <div>
+            <div className="mb-2 block">
+              <Label
+                htmlFor="bookDescription"
+                value="Book Description"
+              />
+            </div>
+            <Textarea
+              id="bookDescription"
+              placeholder="Book Description"
+              required
+              type="text"
+              name='bookDescription'
+              className='w-full'
+              rows={4}
+              defaultValue={bookDescription}
+            />
+          </div>
+
+
+        {/* book pdf url */}
+          <div className='flex gap-8'>
+            <div className='lg:w-1/2'>
+            <div className="mb-2 block">
+            <Label
+              htmlFor="bookPDFURL"
+              value="Book PDF Link"
+            />
+          </div>
+          <TextInput
+            id="bookPDFURL"
+            placeholder="Paste Book PDF URL here"
+            required
+            type="text"
+            name='bookPDFURL'
+            className='w-full'
+            defaultValue={bookPDFURL}
+          />
+          </div>
+          <div className='lg:w-1/2'>
+            <div className="mb-2 block">
+            <Label
+              htmlFor="price"
+              value="Price $"
+            />
+          </div>
+           <TextInput
+            id="price"
+            placeholder="Price"
+            required
+            type="text"
+            name='price'
+            className='w-full'
+            defaultValue={price}
+          />
+          </div>
+        </div>
+
+
+          {/* Submit btn */}
+          <Button type="submit" className='mt-5'>
+            Upload book
+          </Button>
+
+        </form>
+      </div>
+    )
+  }
+
+export default EditBooks
